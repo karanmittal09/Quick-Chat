ca8703dea8df12e9d13296abce36140f59df1d22
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-05-21 00:14:47 +0530

    Add Main and Sidebar components with styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,32 +1,59 @@
-import React from 'react'
-import './Sidebar.css'
-import {assets} from '../../assets/assets'
+import "./sidebar.css";
+import { assets } from "../../assets/assets";
+import { useState } from "react";
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
+	const [extended, setExtended] = useState(false);
+	
+	return (
+		<div className="sidebar">
+			<div className="top">
+				<img
+					src={assets.menu_icon}
+					className="menu"
+					alt="menu-icon"
+					onClick={() => {
+						setExtended((prev) => !prev);
+					}}
+				/>
+				<div className="new-chat">
+					<img src={assets.plus_icon} alt="" onClick={()=>{
+                        newChat()
+                    }} />
+					{extended ? <p>New Chat</p> : null}
+				</div>
+				{extended ? (
+					<div className="recent">
+						<p className="recent-title">Recent</p>
+						{prevPrompts.map((item, index) => {
+							return (
+								<div onClick={()=>{
+                                    loadPreviousPrompt(item)
+                                }} className="recent-entry">
+									<img src={assets.message_icon} alt="" />
+									<p>{item.slice(0, 18)}...</p>
+								</div>
+							);
+						})}
+					</div>
+				) : null}
+			</div>
+			<div className="bottom">
+				<div className="bottom-item recent-entry">
+					<img src={assets.question_icon} alt="" />
+					{extended ? <p>Help desk</p> : null}
+				</div>
+				<div className="bottom-item recent-entry">
+					<img src={assets.history_icon} alt="" />
+					{extended ? <p>History</p> : null}
+				</div>
+				<div className="bottom-item recent-entry">
+					<img src={assets.setting_icon} alt="" />
+					{extended ? <p>Settings</p> : null}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-    <div className='top'>
-      <img className='menu' src={assets.menu_icon} alt=""/>
-      <div className="new-chat">
-        <img src={assets.plus_icon} alt="" />
-        <p>New Chat</p>
-      </div>
-      <div className="recent">
-        <p className='recent-title'>Recent</p>
-        <div className="recent-entry">
-          <img src={assets.message_icon} alt=""/> 
-            <p>React is what...</p>
-        </div>
-      </div>
-    </div>
-
-    <div className='bottom'></div>
-
-    </div>
-
-    
-  )
-}
-
-export default Sidebar
+export default Sidebar;
